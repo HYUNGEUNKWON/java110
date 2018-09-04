70242cd9d30c9ba27375a16ad09bff67557ad474
HYUNGEUN KWON <floyd9999999@gmail.com>
2018-09-04 14:57:05 +0900

    메서드 적용하여 코드 분리

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
 
 public class App {
+    static String[] names = new String[100];
+    static String[] emails = new String[100];
+    static String[] passwords = new String[100];
+
+    static int index = 0;
+
+    static Scanner keyIn = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String[] names = new String[100];
-        String[] emails = new String[100];
-        String[] passwords = new String[100];
 
-        int index = 0;
+        inputMembers();
 
-        // 1) 키보드 입력을 처리할 객체 준비
-        Scanner keyIn = new Scanner(System.in);
+        printMembers();
 
-        // 2) 사용자로부터 회원 정보 입력 받기
+        keyIn.close();
+    }
 
+    static void printMembers() {
+        for(int i = 0; i < index; i++){
+            System.out.printf("%s, %s, %s\n", names[i], emails[i], passwords[i]);
+        }
+    }
+    static void inputMembers() {
         while(true) {
             System.out.print("이름? ");
             names[index] = keyIn.nextLine();
@@ -24,16 +35,11 @@ public class App {
             passwords[index] = keyIn.nextLine();
 
             index++;
-            
+
             System.out.print("계속 하시겠습니까? (Y/n)");
             String answer = keyIn.nextLine();
             if(answer.toLowerCase().equals("n"))
                 break;
         }
-        for(int i = 0; i < index; i++){
-            System.out.printf("%s, %s, %s\n", names[i], emails[i], passwords[i]);
-        }
-        
-        keyIn.close();
     }
 }
