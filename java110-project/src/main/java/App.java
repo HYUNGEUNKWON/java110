3e5e8a75eaaf09a47894ec0436504b0f41753c3c
HYUNGEUN KWON <floyd9999999@gmail.com>
2018-09-05 11:01:49 +0900

    강사관리 기능추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -35,6 +35,7 @@ public class App {
         protected String school;
         protected boolean working;
         protected String tel;
+        
         public String getSchool() {
             return school;
         }
@@ -57,9 +58,38 @@ public class App {
         
     }
     
+    static class Teacher extends Member {
+        protected String tel;
+        protected int pay;
+        protected String subjects;
+        
+        public String getTel() {
+            return tel;
+        }
+        public void setTel(String tel) {
+            this.tel = tel;
+        }
+        public int getPay() {
+            return pay;
+        }
+        public void setPay(int pay) {
+            this.pay = pay;
+        }
+        public String getSubjects() {
+            return subjects;
+        }
+        public void setSubjects(String subjects) {
+            this.subjects = subjects;
+        }
+        
+        
+    }
+    
     static Student[] students = new Student[100];
+    static Teacher[] teachers = new Teacher[100];
 
-    static int index = 0;
+    static int studentIndex = 0;
+    static int teacherIndex = 0;
 
     static Scanner keyIn = new Scanner(System.in);
 
@@ -69,6 +99,8 @@ public class App {
 
             if(menu.equals("1")) {
                 serviceStudentMenu();
+            } else if(menu.equals("2")) {
+                serviceTeacherMenu();
             } else if(menu.equals("0")){
                 System.out.println("안녕히 가세요!");
                 break;
@@ -92,6 +124,22 @@ public class App {
         }
     }
 
+    private static void serviceTeacherMenu() {
+        while(true) {
+            System.out.print("강사 관리> ");
+            String command = keyIn.nextLine();
+            if (command.equals("list")) {
+                printTeachers();
+            } else if(command.equals("add")){
+                inputTeachers();
+            } else if(command.equals("quit")) {
+                break;
+            } else {
+                System.out.println("유효하지 않은 명령입니다.");
+            }
+        }
+    }
+    
     private static String promptMenu() {
         //사용자로부터 메뉴를 입력 받기
         System.out.println("[메뉴]");
@@ -121,7 +169,7 @@ public class App {
     static void printStudents() {
         int count = 0;
         for(Student s : students){
-            if(count++ == index)
+            if(count++ == studentIndex)
                 break;
             System.out.printf("%s, %s, %s, %s, %b, %s\n",
                     s.getNames(), 
@@ -133,6 +181,22 @@ public class App {
 
         }
     }
+    
+    static void printTeachers() {
+        int count = 0;
+        for(Teacher s : teachers){
+            if(count++ == teacherIndex)
+                break;
+            System.out.printf("%s, %s, %s, %s, %d, [%s]\n",
+                    s.getNames(), 
+                    s.getEmails(),
+                    s.getPasswords(),
+                    s.getTel(),
+                    s.getPay(),
+                    s.getSubjects());
+        }
+    }
+    
     static void inputStudents() {
         while(true) {
             Student m = new Student();
@@ -148,13 +212,43 @@ public class App {
             System.out.print("최종학력? ");
             m.setSchool(keyIn.nextLine());
 
-            System.out.print("재직여부? ");
+            System.out.print("재직여부?(true/false) ");
             m.setWorking(Boolean.parseBoolean(keyIn.nextLine()));
             
             System.out.print("전화번호? ");
             m.setTel(keyIn.nextLine());
             
-            students[index++] = m;
+            students[studentIndex++] = m;
+
+            System.out.print("계속 하시겠습니까? (Y/n)");
+            String answer = keyIn.nextLine();
+            if(answer.toLowerCase().equals("n"))
+                break;
+        }
+    }
+    
+    static void inputTeachers() {
+        while(true) {
+            Teacher m = new Teacher();
+            System.out.print("이름? ");
+            m.setNames(keyIn.nextLine());
+
+            System.out.print("이메일? ");
+            m.setEmails(keyIn.nextLine());
+
+            System.out.print("암호? ");
+            m.setPasswords(keyIn.nextLine());
+
+            System.out.print("전화번호? ");
+            m.setTel(keyIn.nextLine());
+
+            System.out.print("시급? ");
+            m.setPay(Integer.parseInt(keyIn.nextLine()));
+            
+            System.out.print("강의과목?(예: 자바, C, C++) ");
+            m.setSubjects(keyIn.nextLine());
+            
+            teachers[teacherIndex++] = m;
 
             System.out.print("계속 하시겠습니까? (Y/n)");
             String answer = keyIn.nextLine();
