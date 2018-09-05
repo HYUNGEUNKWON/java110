80185110c64f6224d0f45391519c200c93207dbc
HYUNGEUN KWON <floyd9999999@gmail.com>
2018-09-05 12:02:48 +0900

    매니저 관리 기능 추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -1,143 +1,30 @@
 import java.util.Scanner;
 
 public class App {
-    
-    //여러 속성의 값을 관리하기 쉽도록 사용자 정의 데이터 타입을 만들어 사용한다.
-    static class Member {
-        protected String names;
-        protected String emails;
-        protected String passwords;
-        
-        // 인스턴스의 메모리를 다루는 operator = setter/getter = accessor = property = message
-        public String getNames() {
-            return names;
-        }
-        public void setNames(String names) {
-            this.names = names;
-        }
-        public String getEmails() {
-            return emails;
-        }
-        public void setEmails(String emails) {
-            this.emails = emails;
-        }
-        public String getPasswords() {
-            return passwords;
-        }
-        public void setPasswords(String passwords) {
-            this.passwords = passwords;
-        }
-        
-        
-    }
-
-    static class Student extends Member {
-        protected String school;
-        protected boolean working;
-        protected String tel;
-        
-        public String getSchool() {
-            return school;
-        }
-        public void setSchool(String school) {
-            this.school = school;
-        }
-        public boolean isWorking() {
-            return working;
-        }
-        public void setWorking(boolean working) {
-            this.working = working;
-        }
-        public String getTel() {
-            return tel;
-        }
-        public void setTel(String tel) {
-            this.tel = tel;
-        }
-        
-        
-    }
-    
-    static class Teacher extends Member {
-        protected String tel;
-        protected int pay;
-        protected String subjects;
-        
-        public String getTel() {
-            return tel;
-        }
-        public void setTel(String tel) {
-            this.tel = tel;
-        }
-        public int getPay() {
-            return pay;
-        }
-        public void setPay(int pay) {
-            this.pay = pay;
-        }
-        public String getSubjects() {
-            return subjects;
-        }
-        public void setSubjects(String subjects) {
-            this.subjects = subjects;
-        }
-        
-        
-    }
-    
-    static Student[] students = new Student[100];
-    static Teacher[] teachers = new Teacher[100];
-
-    static int studentIndex = 0;
-    static int teacherIndex = 0;
 
     static Scanner keyIn = new Scanner(System.in);
 
     public static void main(String[] args) {
+        
+        StudentController.keyIn = keyIn;
+        TeacherController.keyIn = keyIn;
+        ManagerController.keyIn = keyIn;
+        
         while(true) {
             String menu = promptMenu();
 
             if(menu.equals("1")) {
-                serviceStudentMenu();
+                StudentController.serviceStudentMenu();
             } else if(menu.equals("2")) {
-                serviceTeacherMenu();
+                TeacherController.serviceTeacherMenu();
+            } else if(menu.equals("3")) {
+                ManagerController.serviceManagerMenu();
             } else if(menu.equals("0")){
                 System.out.println("안녕히 가세요!");
                 break;
             }
         }
         keyIn.close();
-    }
-    private static void serviceStudentMenu() {
-        while(true) {
-            System.out.print("학생 관리> ");
-            String command = keyIn.nextLine();
-            if (command.equals("list")) {
-                printStudents();
-            } else if(command.equals("add")){
-                inputStudents();
-            } else if(command.equals("quit")) {
-                break;
-            } else {
-                System.out.println("유효하지 않은 명령입니다.");
-            }
-        }
-    }
-
-    private static void serviceTeacherMenu() {
-        while(true) {
-            System.out.print("강사 관리> ");
-            String command = keyIn.nextLine();
-            if (command.equals("list")) {
-                printTeachers();
-            } else if(command.equals("add")){
-                inputTeachers();
-            } else if(command.equals("quit")) {
-                break;
-            } else {
-                System.out.println("유효하지 않은 명령입니다.");
-            }
-        }
     }
     
     private static String promptMenu() {
@@ -166,94 +53,4 @@ public class App {
 
     }
 
-    static void printStudents() {
-        int count = 0;
-        for(Student s : students){
-            if(count++ == studentIndex)
-                break;
-            System.out.printf("%s, %s, %s, %s, %b, %s\n",
-                    s.getNames(), 
-                    s.getEmails(),
-                    s.getPasswords(),
-                    s.getSchool(),
-                    s.isWorking(),
-                    s.getTel());
-
-        }
-    }
-    
-    static void printTeachers() {
-        int count = 0;
-        for(Teacher s : teachers){
-            if(count++ == teacherIndex)
-                break;
-            System.out.printf("%s, %s, %s, %s, %d, [%s]\n",
-                    s.getNames(), 
-                    s.getEmails(),
-                    s.getPasswords(),
-                    s.getTel(),
-                    s.getPay(),
-                    s.getSubjects());
-        }
-    }
-    
-    static void inputStudents() {
-        while(true) {
-            Student m = new Student();
-            System.out.print("이름? ");
-            m.setNames(keyIn.nextLine());
-
-            System.out.print("이메일? ");
-            m.setEmails(keyIn.nextLine());
-
-            System.out.print("암호? ");
-            m.setPasswords(keyIn.nextLine());
-
-            System.out.print("최종학력? ");
-            m.setSchool(keyIn.nextLine());
-
-            System.out.print("재직여부?(true/false) ");
-            m.setWorking(Boolean.parseBoolean(keyIn.nextLine()));
-            
-            System.out.print("전화번호? ");
-            m.setTel(keyIn.nextLine());
-            
-            students[studentIndex++] = m;
-
-            System.out.print("계속 하시겠습니까? (Y/n)");
-            String answer = keyIn.nextLine();
-            if(answer.toLowerCase().equals("n"))
-                break;
-        }
-    }
-    
-    static void inputTeachers() {
-        while(true) {
-            Teacher m = new Teacher();
-            System.out.print("이름? ");
-            m.setNames(keyIn.nextLine());
-
-            System.out.print("이메일? ");
-            m.setEmails(keyIn.nextLine());
-
-            System.out.print("암호? ");
-            m.setPasswords(keyIn.nextLine());
-
-            System.out.print("전화번호? ");
-            m.setTel(keyIn.nextLine());
-
-            System.out.print("시급? ");
-            m.setPay(Integer.parseInt(keyIn.nextLine()));
-            
-            System.out.print("강의과목?(예: 자바, C, C++) ");
-            m.setSubjects(keyIn.nextLine());
-            
-            teachers[teacherIndex++] = m;
-
-            System.out.print("계속 하시겠습니까? (Y/n)");
-            String answer = keyIn.nextLine();
-            if(answer.toLowerCase().equals("n"))
-                break;
-        }
-    }
 }
---
+++ java110-project/src/main/java/Member.java
@@ -0,0 +1,30 @@
+
+public class Member {
+    protected String names;
+    protected String emails;
+    protected String passwords;
+
+    // 인스턴스의 메모리를 다루는 operator = setter/getter = accessor = property = message
+    public String getNames() {
+        return names;
+    }
+    public void setNames(String names) {
+        this.names = names;
+    }
+    public String getEmails() {
+        return emails;
+    }
+    public void setEmails(String emails) {
+        this.emails = emails;
+    }
+    public String getPasswords() {
+        return passwords;
+    }
+    public void setPasswords(String passwords) {
+        this.passwords = passwords;
+    }
+
+
+}
+
+
