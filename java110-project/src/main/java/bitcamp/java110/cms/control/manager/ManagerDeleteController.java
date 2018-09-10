4f274bfba3e1c126619e811afadd9609706c8e96
HYUNGEUN KWON <floyd9999999@gmail.com>
2018-09-10 14:33:32 +0900

    강사, 매니저 관리에 command 패턴 적용

---
+++ java110-project/src/main/java/bitcamp/java110/cms/control/manager/ManagerDeleteController.java
@@ -0,0 +1,25 @@
+package bitcamp.java110.cms.control.manager;
+
+import java.util.Scanner;
+
+import bitcamp.java110.cms.App;
+import bitcamp.java110.cms.annotation.Component;
+import bitcamp.java110.cms.annotation.RequestMapping;
+import bitcamp.java110.cms.domain.Manager;
+
+@Component
+public class ManagerDeleteController {
+    @RequestMapping("manager/delete")
+    public void deleteManager(Scanner keyIn) {
+        System.out.print("삭제할 번호? ");
+        int no = Integer.parseInt(keyIn.next());
+
+        if (no < 0 || no >= App.managers.size()) {
+            System.out.println("무효한 번호입니다.");
+            return;
+        }
+        App.managers.remove(no);
+        
+        System.out.println("삭제하였습니다.");
+    }
+}
