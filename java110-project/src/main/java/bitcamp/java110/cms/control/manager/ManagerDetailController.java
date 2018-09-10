b5682e9bbf8f577012edd38966d1be6ac0584d41
HYUNGEUN KWON <floyd9999999@gmail.com>
2018-09-10 16:50:06 +0900

    강사, 매니저 관리에 DAO 적용하기

--- java110-project/src/main/java/bitcamp/java110/cms/control/manager/ManagerDetailController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/control/manager/ManagerDetailController.java
@@ -8,27 +8,24 @@ import bitcamp.java110.cms.annotation.RequestMapping;
 import bitcamp.java110.cms.domain.Manager;
 
 @Component
-public class ManagerDetailController {
-    
+public class ManagerDetailController { 
 
     @RequestMapping("manager/detail")
     public void detail(Scanner keyIn) {
-        System.out.print("조회할 번호? ");
-        int no = Integer.parseInt(keyIn.next());
-
-        if (no < 0 || no >= App.managers.size()) {
-            System.out.println("무효한 번호입니다.");
+        System.out.print("조회할 매니저의 이메일? ");
+        String email = keyIn.nextLine();
+        Manager m = App.managerDao.findByEmail(email);
+        
+        if (m == null) {
+            System.out.println("해당 이메일의 매니저가 없습니다!");
             return;
         }
         
-        Manager manager = App.managers.get(no);
-        
-        System.out.printf("이름: %s\n", manager.getName());
-        System.out.printf("이메일: %s\n", manager.getEmail());
-        System.out.printf("암호: %s\n", manager.getPassword());
-        System.out.printf("전화: %s\n", manager.getTel());
-        System.out.printf("직책: %s\n", manager.getPosition());
+        System.out.printf("이름: %s\n", m.getName());
+        System.out.printf("이메일: %s\n", m.getEmail());
+        System.out.printf("암호: %s\n", m.getPassword());
+        System.out.printf("직위: %s\n", m.getPosition());
+        System.out.printf("전화: %s\n", m.getTel());
     }
-
+    
 }
-
--- java110-project/src/main/java/bitcamp/java110/cms/control/manager/ManagerListController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/control/manager/ManagerListController.java
@@ -1,5 +1,6 @@
 package bitcamp.java110.cms.control.manager;
 
+import java.util.List;
 import java.util.Scanner;
 
 import bitcamp.java110.cms.App;
@@ -8,19 +9,19 @@ import bitcamp.java110.cms.annotation.RequestMapping;
 import bitcamp.java110.cms.domain.Manager;
 
 @Component
-public class ManagerListController {
-    
+public class ManagerListController { 
+
     @RequestMapping("manager/list")
     public void list(Scanner keyIn) {
-        for(int i = 0; i < App.managers.size(); i++){
-            Manager s = App.managers.get(i);
+        List<Manager> list = App.managerDao.findAll();
+        for (Manager s : list) {
             System.out.printf("%s, %s, %s, %s, %s\n",
                     s.getName(), 
-                    s.getEmail(),
-                    s.getPassword(),
+                    s.getEmail(), 
+                    s.getPassword(), 
                     s.getTel(),
                     s.getPosition());
         }
     }
-
+    
 }
