31bdeac70b3cf016bc76fa4eb16dcd28393095f3
HYUNGEUN KWON <floyd9999999@gmail.com>
2018-09-13 17:34:25 +0900

    매니저 CRUD를 JDBC로 완성

--- java110-project/src/main/java/bitcamp/java110/cms/dao/ManagerDao.java
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/ManagerDao.java
@@ -7,6 +7,12 @@ import bitcamp.java110.cms.domain.Manager;
 public interface ManagerDao {
     public int insert(Manager manager) throws MandatoryValueDaoException, DuplicationDaoException;
     public List<Manager> findAll();
+    
     public Manager findByEmail(String email);
-    public int delete(String email);
+    default Manager findByNo(int no) {return null;}
+    
+    default int delete(String email) {return 0;}
+    
+    // 기존 구현체에는 영향을 안끼치는 새로운 규칙 (default)
+    default int deleteByNo(int no) {return 0;}
 }
