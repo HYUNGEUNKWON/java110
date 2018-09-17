e29c1c4b9f2a59fce8d266b0adb339df799b5a4f
HYUNGEUN KWON <floyd9999999@gmail.com>
2018-09-17 13:07:08 +0900

    멀티스레드 적용

--- java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
+++ java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
@@ -13,21 +13,21 @@ import bitcamp.java110.cms.context.RequestMappingHandlerMapping;
 import bitcamp.java110.cms.context.RequestMappingHandlerMapping.RequestMappingHandler;
 
 public class ServerApp {
-    
+
     ClassPathXmlApplicationContext iocContainer;
     RequestMappingHandlerMapping requestHandlerMap;
-    
+
     public ServerApp() throws Exception {
         createIoCContainer();
         logBeansOfContainer();
         processRequestMappingAnnotation();
     }
-    
+
     private void createIoCContainer() {
         iocContainer = new ClassPathXmlApplicationContext(
                 "bitcamp/java110/cms/conf/application-context.xml");
     }
-    
+
     private void processRequestMappingAnnotation() {
         requestHandlerMap = new RequestMappingHandlerMapping();
         String[] names = iocContainer.getBeanDefinitionNames();
@@ -36,7 +36,7 @@ public class ServerApp {
             requestHandlerMap.addMapping(obj);
         }
     }
-    
+
     private void logBeansOfContainer() {
         System.out.println("------------------------");
         String[] nameList = iocContainer.getBeanDefinitionNames();
@@ -45,15 +45,35 @@ public class ServerApp {
         }
         System.out.println("------------------------");
     }
-    
+
     public void service() throws Exception {
         // 클라이언트 연결을 기다리는 서버 소켓 준비
         ServerSocket serverSocket = new ServerSocket(8888);
         System.out.println("서버 실행 중...");
-        
+
         while (true) {
+            Socket socket = serverSocket.accept();
+            RequestWorker worker = new RequestWorker(socket);
+            new Thread(worker).start();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ServerApp serverApp = new ServerApp();
+        serverApp.service();
+    }
+
+    class RequestWorker implements Runnable {
+        Socket socket;
+
+        public RequestWorker(Socket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            // 이 메서드에 "main" 스레드에서 분리하여 독립적으로 수행할 코드를 둔다.
             try (
-                    Socket socket = serverSocket.accept();
+                    Socket socket = this.socket;
                     PrintWriter out = new PrintWriter(
                             new BufferedOutputStream(
                                     socket.getOutputStream()));
@@ -63,7 +83,7 @@ public class ServerApp {
                     ) {
                 System.out.println(in.readLine());
                 out.println("OK:강사"); out.flush();
-                
+
                 while (true) {
                     String requestLine = in.readLine();
                     if (requestLine.equals("EXIT")) {
@@ -72,10 +92,10 @@ public class ServerApp {
                         out.flush();
                         break;
                     }
-                    
+
                     // 요청 객체 준비
                     Request request = new Request(requestLine);
-                    
+
                     // 응답 객체 준비
                     Response response = new Response(out);
 
@@ -87,11 +107,11 @@ public class ServerApp {
                         out.flush();
                         continue;
                     }
-                    
+
                     try {
                         // 요청 핸들러 호출
                         mapping.getMethod().invoke(mapping.getInstance(), request, response);
-                        
+
                     } catch (Exception e) {
                         e.printStackTrace();
                         out.println("요청 처리 중에 오류가 발생했습니다.");
@@ -99,16 +119,14 @@ public class ServerApp {
                     out.println();
                     out.flush();
                 }
-                
+
+            } catch (Exception e) {// try
+                System.out.println(e.getMessage());
             }
-        }
-    }
-    
-    public static void main(String[] args) throws Exception {
-        ServerApp serverApp = new ServerApp();
-        serverApp.service();
-    }
-}
+        } // run()
+    } // RequestWorker class
+
+} // ServerApp class
 
 
 
